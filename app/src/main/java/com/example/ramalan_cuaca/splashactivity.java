88a5ab0b1be620a9e9d1ea34cf9df57b949ce05d
febrigpratama <febrigtama@gmail.com>
2019-06-09 23:04:25 +0700

    coba chris

---
+++ app/src/main/java/com/example/ramalan_cuaca/splashactivity.java
@@ -0,0 +1,28 @@
+package com.example.ramalan_cuaca;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splashactivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashactivity);
+        Thread thread = new Thread() {
+
+            public void run () {
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace ();
+                } finally {
+                    startActivity(new Intent(splashactivity.this , MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
