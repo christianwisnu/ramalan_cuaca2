44d99e1ffe7f7f1761b6f1af5f7808519f10d072
febrigpratama <febrigtama@gmail.com>
2019-06-13 00:22:00 +0700

    tes commit

--- app/src/main/java/com/example/ramalan_cuaca/cuaca_menu.java
+++ app/src/main/java/com/example/ramalan_cuaca/cuaca_menu.java
@@ -3,7 +3,7 @@ package com.example.ramalan_cuaca;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class cuaca_menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
